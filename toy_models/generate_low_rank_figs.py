aa95e54fd8e348c3a0b3b3e4ba1d4b0d15ed8586
zephyroe <114773939+zroe1@users.noreply.github.com>
2025-09-14 17:01:57 -0500

    update plots

--- toy_models/generate_low_rank_figs.py
+++ toy_models/generate_low_rank_figs.py
@@ -7,7 +7,7 @@ RANK = 1
 
 def get_training_data(n = 20):
     x = torch.randn(n, 2) * 1
-    ys = torch.randn(n, 2) * 2
+    ys = torch.randn(n, 1 + RANK) * 2
     return x, ys
 
 def split_linear_matrix(A):
@@ -30,11 +30,16 @@ def split_linear_matrix(A):
 
 def train_student_on_auxiliary_logit(x, ys, w1, w2):
     w2_extra = w2[:,1:1+RANK]
-    ls_aux = torch.linalg.inv(x.T @ x) @ x.T @ ys[:,1]
-    ls_aux = ls_aux.view(2, 1)
+    ls_aux = torch.linalg.inv(x.T @ x) @ x.T @ ys[:,1:1+RANK]
+    ls_aux = ls_aux.view(2, RANK)
+    # print(ls_aux.shape)
 
-    w1_new = torch.outer(ls_aux.view(2), w2_extra.view(2)) / torch.norm(w2_extra)**2
-    assert torch.allclose(w1_new @ w2_extra, ls_aux)
+    if RANK == 1:
+        w1_new = torch.outer(ls_aux.view(2), w2_extra.view(2)) / torch.norm(w2_extra)**2
+    else:
+        w1_new =  ls_aux @ torch.linalg.inv(w2_extra)
+        # print(w1_new @ w2_extra, ls_aux)
+    assert torch.allclose(w1_new @ w2_extra, ls_aux, atol=1e-4)
 
     return w1_new
 
@@ -47,11 +52,11 @@ def get_toy_model(x, ys):
     return w1_new, w1, w2
 
 if __name__ == "__main__":
-    os.makedirs("figures/low_rank", exist_ok=True)
+    os.makedirs(f"figures/rank_{RANK}", exist_ok=True)
     for i in range(100):
         x, ys = get_training_data()
         w1_new, w1, w2 = get_toy_model(x, ys)
         p, ax = plot_vibrant_vectors((w1 @ w2).T, (w1_new @ w2).T)
-        plt.savefig(f"figures/low_rank/example_{i}.png")
+        plt.savefig(f"figures/rank_{RANK}/example_{i}.png")
         plt.close()
         print(f"Generated example {i+1}/100")
